37f9dd866d60e1892e2d71c1269eed7930c4bc4b
tigger <fronttigger@gmail.com>
2023-01-17 22:44:35 +0900

    feat: 필요 없는 주석 제거

--- rules/base.js
+++ rules/base.js
@@ -2,21 +2,6 @@
 module.exports = {
   rules: {
     /**
-     * @description 콘솔 금지
-     * @link https://eslint.org/docs/latest/rules/no-console
-     */
-    // 'no-console': 'warn',
-    /**
-     * @description 사용하지 않는 변수 사용
-     * @link https://eslint.org/docs/latest/rules/no-unused-vars
-     */
-    // 'no-unused-vars': 'error',
-    /**
-     * @description 빈 함수 작성 금지
-     * @link https://eslint.org/docs/latest/rules/no-empty-function
-     */
-    // 'no-empty-function': 'error',
-    /**
      * @description 세미 콜론 여부
      * @link https://eslint.org/docs/latest/rules/semi
      */
