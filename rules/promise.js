12df623fcf54e8b6183b09c859428b8ba4ce643f
tigger <fronttigger@gmail.com>
2023-01-18 22:08:43 +0900

    feat: BOWL 기준 린트 수정 & postcss 대응

--- rules/promise.js
+++ rules/promise.js
@@ -9,6 +9,6 @@ module.exports = {
      * @description Promise 보다 Async/Await 선호
      * @link https://github.com/eslint-community/eslint-plugin-promise/blob/main/docs/rules/prefer-await-to-then.md
      */
-    'promise/prefer-await-to-then': 'error',
+    'promise/prefer-await-to-then': 'off',
   },
 }
