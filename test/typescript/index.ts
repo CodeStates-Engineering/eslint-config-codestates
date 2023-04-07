08891527c353c541d5c9cbc2962a626c687ead97
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-04-07 16:40:15 +0900

    chore: package 업데이트
    - stylelint-config-prettier 제거

--- test/typescript/index.ts
+++ test/typescript/index.ts
@@ -11,3 +11,84 @@
  *  age: number
  * }
  */
+
+/**
+ * 
+ * 
+ * - The "at-rule-name-case" rule is deprecated.
+ - The "at-rule-name-newline-after" rule is deprecated.
+ - The "at-rule-semicolon-space-before" rule is deprecated.
+ - The "at-rule-name-space-after" rule is deprecated.
+ - The "at-rule-semicolon-newline-after" rule is deprecated.
+ - The "block-closing-brace-empty-line-before" rule is deprecated.
+ - The "block-closing-brace-newline-after" rule is deprecated.
+ - The "block-closing-brace-newline-before" rule is deprecated.
+ - The "block-closing-brace-space-after" rule is deprecated.
+ - The "block-closing-brace-space-before" rule is deprecated.
+ - The "block-opening-brace-newline-after" rule is deprecated.
+ - The "block-opening-brace-newline-before" rule is deprecated.
+ - The "block-opening-brace-space-after" rule is deprecated.
+ - The "block-opening-brace-space-before" rule is deprecated.
+ - The "color-hex-case" rule is deprecated.
+ - The "declaration-bang-space-after" rule is deprecated.
+ - The "declaration-bang-space-before" rule is deprecated.
+ - The "declaration-block-semicolon-newline-after" rule is deprecated.
+ - The "declaration-block-semicolon-newline-before" rule is deprecated.
+ - The "declaration-block-semicolon-space-after" rule is deprecated.
+ - The "declaration-block-semicolon-space-before" rule is deprecated.
+ - The "declaration-block-trailing-semicolon" rule is deprecated.
+ - The "declaration-colon-newline-after" rule is deprecated.
+ - The "declaration-colon-space-after" rule is deprecated.
+ - The "declaration-colon-space-before" rule is deprecated.
+ - The "function-comma-newline-after" rule is deprecated.
+ - The "function-comma-newline-before" rule is deprecated.
+ - The "function-comma-space-after" rule is deprecated.
+ - The "function-comma-space-before" rule is deprecated.
+ - The "function-max-empty-lines" rule is deprecated.
+ - The "function-parentheses-newline-inside" rule is deprecated.
+ - The "function-parentheses-space-inside" rule is deprecated.
+ - The "function-whitespace-after" rule is deprecated.
+ - The "linebreaks" rule is deprecated.
+ - The "max-empty-lines" rule is deprecated.
+ - The "max-line-length" rule is deprecated.
+ - The "media-feature-colon-space-after" rule is deprecated.
+ - The "media-feature-colon-space-before" rule is deprecated.
+ - The "media-feature-name-case" rule is deprecated.
+ - The "media-feature-parentheses-space-inside" rule is deprecated.
+ - The "media-feature-range-operator-space-after" rule is deprecated.
+ - The "media-feature-range-operator-space-before" rule is deprecated.
+ - The "media-query-list-comma-newline-after" rule is deprecated.
+ - The "media-query-list-comma-newline-before" rule is deprecated.
+ - The "media-query-list-comma-space-after" rule is deprecated.
+ - The "media-query-list-comma-space-before" rule is deprecated.
+ - The "no-empty-first-line" rule is deprecated.
+ - The "no-eol-whitespace" rule is deprecated.
+ - The "no-extra-semicolons" rule is deprecated.
+ - The "no-missing-end-of-source-newline" rule is deprecated.
+ - The "number-leading-zero" rule is deprecated.
+ - The "number-no-trailing-zeros" rule is deprecated.
+ - The "property-case" rule is deprecated.
+ - The "selector-attribute-brackets-space-inside" rule is deprecated.
+ - The "selector-attribute-operator-space-after" rule is deprecated.
+ - The "selector-attribute-operator-space-before" rule is deprecated.
+ - The "selector-combinator-space-after" rule is deprecated.
+ - The "selector-combinator-space-before" rule is deprecated.
+ - The "selector-descendant-combinator-no-non-space" rule is deprecated.
+ - The "selector-list-comma-newline-after" rule is deprecated.
+ - The "selector-list-comma-newline-before" rule is deprecated.
+ - The "selector-list-comma-space-after" rule is deprecated.
+ - The "selector-list-comma-space-before" rule is deprecated.
+ - The "selector-max-empty-lines" rule is deprecated.
+ - The "selector-pseudo-class-case" rule is deprecated.
+ - The "selector-pseudo-class-parentheses-space-inside" rule is deprecated.
+ - The "selector-pseudo-element-case" rule is deprecated.
+ - The "string-quotes" rule is deprecated.
+ - The "unicode-bom" rule is deprecated.
+ - The "unit-case" rule is deprecated.
+ - The "value-list-comma-newline-after" rule is deprecated.
+ - The "value-list-comma-newline-before" rule is deprecated.
+ - The "value-list-comma-space-after" rule is deprecated.
+ - The "value-list-comma-space-before" rule is deprecated.
+ - The "value-list-max-empty-lines" rule is deprecated.
+ - The "indentation" rule is deprecated.
+ */
